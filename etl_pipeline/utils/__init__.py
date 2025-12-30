1fed80fd31fd42272c473eff95c32a2692cdcfc8
Akash Pandit <34327539+akash-pandit@users.noreply.github.com>
2025-12-30 14:10:09 -0800

    df construction logic moved to utils

--- etl_pipeline/utils/__init__.py
+++ etl_pipeline/utils/__init__.py
@@ -2,6 +2,7 @@
 
 from .benchmarking import timer
 from .dnf_converter import to_dnf
+from .generate_articulations import extract_articulations_lazy
 from .generate_glossary import create_glossary
 from .generate_schema import load_full_schema
 
@@ -9,6 +10,7 @@ from .generate_schema import load_full_schema
 __all__ = [
     'timer',
     'to_dnf',
+    'extract_articulations_lazy',
     'create_glossary',
     'load_full_schema'
 ]
